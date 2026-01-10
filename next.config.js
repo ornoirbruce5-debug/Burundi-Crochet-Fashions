968eaca36767fb96b7b15ce7950a382585bdecab
Intimatelures <ornoirbruce5@gmail.com>
2026-01-10 15:29:24 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
+  // Uncomment and set basePath/assetPrefix if deploying to repo subpath
+  // basePath: '/burundi-crochet-fashions',
+  // assetPrefix: '/burundi-crochet-fashions/',
+};
 
+module.exports = nextConfig;
