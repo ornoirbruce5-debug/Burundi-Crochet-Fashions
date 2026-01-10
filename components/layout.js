251742bd18ef7e13b473759d0bab5a9315d810ed
Intimatelures <ornoirbruce5@gmail.com>
2026-01-10 15:45:29 +0200

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -1,1 +1,12 @@
+import Header from './Header';
+import Footer from './Footer';
 
+export default function Layout({ children, lang, onToggleLang }) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header lang={lang} onToggleLang={onToggleLang} />
+      <div className="flex-1">{children}</div>
+      <Footer />
+    </div>
+  );
+}
